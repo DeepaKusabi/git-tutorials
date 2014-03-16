3367ae5e97917d13b88f8604939a8224d9dee88c
Bruce <bruce@denofprogramming.com>
2014-03-16 18:13:10 +0000

    Initial version commit.

---
+++ gitsample/src/main/java/gitsample/HelloWorld.java
@@ -0,0 +1,12 @@
+package gitsample;
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+	System.out.println("Hello World, code held in a Git Repository");
+
+    }
+
+}
